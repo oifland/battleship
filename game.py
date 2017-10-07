f70949c306cd7ed3283b9d5efb5acde9e17efba5
Orion Ifland <ifland@gmail.com>
2017-10-07 14:52:43 -0700

    Add board

--- game.py
+++ game.py
@@ -1,1 +1,23 @@
 print "Welcome to Battleship"
+
+board = {
+      # 0 1 2 3 4 5 6 7
+  'A': [0,0,0,1,0,0,0,0],
+  'B': [0,0,0,1,0,0,0,0],
+  'C': [0,0,0,1,0,0,0,0],
+  'D': [0,0,0,1,0,0,0,0],
+  'E': [0,0,0,0,0,0,1,0],
+  'F': [1,1,1,1,1,0,1,0],
+  'G': [0,0,0,0,0,0,0,0],
+  'H': [0,0,0,1,1,1,0,0]
+}
+
+print "Here is the board:"
+print board['A']
+print board['B']
+print board['C']
+print board['D']
+print board['E']
+print board['F']
+print board['G']
+print board['H']
